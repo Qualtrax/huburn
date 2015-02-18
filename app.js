39703217d1a001764f784d5169c7828d63135038
Jordan Ramey <jramey@qualtrax.com>
2015-02-18 16:50:30 -0500

    [GH36] Add Burnup * Create gitHubService * Only storing the repo name now instead of the whole route. * Renamed partials folder to templates/ * Added a Watch to the repo in the navigationService * Refactored controllers and directives for better readability * Extracted the availability service * Added loading icon while github requests are processed

--- app.js
+++ app.js
@@ -10,26 +10,31 @@ var oauth = require('./lib/oauth-github');
 var github = require('./lib/github');
 var static = require('./lib/static');
 
-http.createServer(function(req, res) {
-  res.writeHead(200, { 'Content-Type': 'text/plain' });
-  res.end('Huburn is served over SSL, change the address to start with "https".');
+http.createServer(function (req, res) {
+    res.writeHead(200, {
+        'Content-Type': 'text/plain'
+    });
+    res.end('Huburn is served over SSL, change the address to start with "https".');
 }).listen(8080);
 
 var app = express();
 
 app.use(session({
-  name: 'huburn.sid',
-  cookie: { expires: false, secure: true },
-  secret: crypto.randomBytes(20).toString('hex'),
-  resave: true,
-  saveUninitialized: true
+    name: 'huburn.sid',
+    cookie: {
+        expires: false,
+        secure: true
+    },
+    secret: crypto.randomBytes(20).toString('hex'),
+    resave: true,
+    saveUninitialized: true
 }));
 
-app.use(function(req, res, next) {
-  if (!req.session.access_token && req.path.slice(0, 7) != '/login/')
-    res.redirect('/login/oauth');
-  else 
-    next();
+app.use(function (req, res, next) {
+    if (!req.session.access_token && req.path.slice(0, 7) != '/login/')
+        res.redirect('/login/oauth');
+    else
+        next();
 });
 
 app.use(static);
@@ -37,8 +42,8 @@ app.use(oauth);
 app.use(github);
 
 var options = {
-  key: fs.readFileSync(__dirname + config.sslKeyPath),
-  cert: fs.readFileSync(__dirname + config.sslCertPath)
+    key: fs.readFileSync(__dirname + config.sslKeyPath),
+    cert: fs.readFileSync(__dirname + config.sslCertPath)
 };
 
 https.createServer(options, app).listen(8443);
--- gruntfile.js
+++ gruntfile.js
@@ -1,27 +1,47 @@
-module.exports = function(grunt) {
-  var shell = require('shelljs');
+module.exports = function (grunt) {
+    var shell = require('shelljs');
 
-  grunt.initConfig({
-    pkg: grunt.file.readJSON('package.json'),
-    protractor: {
-      options: {
-        configFile: "protractor.conf.js",
-        keepAlive: true,
-        noColor: false,
-        args: {}
-      },
-      chrome: {
-        options: {
-          args: {
-            browser: "chrome"
-          }
-        }
-      },
-    },
-    protractor_webdriver: {
-      huburn: {},
-    },
-  });
+    grunt.initConfig({
+        pkg: grunt.file.readJSON('package.json'),
+        protractor: {
+            options: {
+                configFile: "protractor.conf.js",
+                keepAlive: true,
+                noColor: false,
+                args: {}
+            },
+            chrome: {
+                options: {
+                    args: {
+                        browser: "chrome"
+                    }
+                }
+            },
+        },
+        less: {
+            production: {
+                options: {
+                    cleancss: true
+                },
+                files: {
+                    "lib/static/static/css/main.css": "lib/static/less/main.less"
+                }
+            }
+        },
+        watch: {
+            less: {
+                files: ['lib/static/less/main.less'],
+                tasks: ['less']
+            }
+        },
+        protractor_webdriver: {
+            huburn: {},
+        },
+    });
 
-  grunt.loadNpmTasks('grunt-protractor-runner');
+    grunt.loadNpmTasks('grunt-contrib-less');
+    grunt.loadNpmTasks('grunt-protractor-runner');
+    grunt.loadNpmTasks('grunt-contrib-watch');
+
+    grunt.registerTask('compile-less', ['watch:less']);
 };
