5ec84e0bd2d86df5eb54ecfdf064b21883178abb
kevPo <kevposton@gmail.com>
2016-03-03 11:51:09 -0500

    [GH1605] Add time metrics to HuBurn * Added proper format to zero-defects and near-misses * Updated angular to 1.4 * Added categories to velocity metrics * Added directive for donuts

--- tests/issue.tests.js
+++ tests/issue.tests.js
@@ -2,21 +2,21 @@ describe("Point value of an issue", function() {
 
   it("should be based on a point label", function() {
     var issue = { labels: [ { name: "bug" }, { name: "points: 1" } ] };
-    expect(getPoints(issue)).toBe(1);
+    expect(getCapitalizablePoints(issue)).toBe(1);
   });
 
   it("is determined by the first point label found", function() {
     var issue = { labels: [ { name: "bug" }, { name: "points: 1" }, { name: "points: 5" } ] };
-    expect(getPoints(issue)).toBe(1);
+    expect(getCapitalizablePoints(issue)).toBe(1);
   });
 
   it("can be more than a single digit", function() {
     var issue = { labels: [ { name: "points: 10" } ] };
-    expect(getPoints(issue)).toBe(10);
+    expect(getCapitalizablePoints(issue)).toBe(10);
   });
 
   it("should be zero for issues without a point label", function() {
     var issue = { labels: [] }
-    expect(getPoints(issue)).toBe(0);
+    expect(getCapitalizablePoints(issue)).toBe(0);
   });
 });
